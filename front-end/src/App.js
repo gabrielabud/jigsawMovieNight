9ca74654e4d1d51ecd93e41a49cff763beee2b61
gabrielabud <gabriela.budeanu@gmail.com>
2018-01-26 10:04:51 +0000

    Fetch API data to APP.js
    we are now able to see the structure of the api

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,16 +3,41 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+     super(props)
+     this.state = {
+       movieslist: [],
+      }
+  }
+
+   componentDidMount() {
+   let self=this;
+
+   fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=121486b23802e0b6735125ff1892f340&language=en-US&page=1")
+     .then(function(results) {
+       return results.json();
+     })
+     .then(function(data){
+
+          console.log(data.results)
+         self.setState({
+           movieslist: data
+         })
+     })
+     .catch(function(error) {
+       console.log(error)
+     });
+ }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Movies App</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+        </div>
       </div>
     );
   }
